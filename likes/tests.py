f10bf1c5d482f3e59a9433d4628fd2063f7f57df
chestnut90 <cowzon90@gmail.com>
2023-10-26 10:52:46 +0900

    :white_check_mark:Test : 게시물 좋아요 api 테스트 #11
    - authenticated 되지 않은 경우Unauthorized(401)
    - authenticated 된 경우 like_count를 증가하며 Ok(200)
    
    Related to #11

--- likes/tests.py
+++ likes/tests.py
@@ -1,1 +1,37 @@
-# Create your tests here.
+from rest_framework.test import APIClient, APITestCase
+
+from posts.models import Post
+from users.models import User
+
+
+class LikeAPITestCase(APITestCase):
+    client = APIClient(enforce_csrf_checks=True)
+    url = "/likes/"
+
+    def setUp(self):
+        self.user = User.objects.create(email="user")
+
+        self.post = Post.objects.create(title="title", post_type="facebook", content="content")
+
+    def test_post_without_auth(self):
+        """logout and post like"""
+
+        self.client.logout()
+
+        url = f"{self.url}{self.post.pk}/"
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_post_with_auth(self):
+        """login and post like"""
+
+        # self.client.force_login(self.user)
+        self.client.force_authenticate(user=self.user)
+
+        url = f"{self.url}{self.post.pk}/"
+
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.post.like_count, Post.objects.first().like_count - 1)
