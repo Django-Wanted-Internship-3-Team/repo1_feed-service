e7ed99b4aa59ca5206963e88e03bb51406b3cd9f
chestnut90 <cowzon90@gmail.com>
2023-10-27 07:35:45 +0900

    :recycle:Refact : likes api에 대한 swagger 적용 #11
    - api 문서화를 위한 swagger 적용
    
    Related to #11

--- likes/views.py
+++ likes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 from rest_framework.views import APIView
 
 from likes.serializers import PostLikeIncrementSerializer
@@ -10,6 +13,13 @@ from posts.models import Post
 class LikesAPIView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    @swagger_auto_schema(
+        operation_summary="게시물에 좋아요",
+        responses={
+            HTTP_200_OK: openapi.Response(description="ok"),
+            HTTP_401_UNAUTHORIZED: openapi.Response(description="unauthorized"),
+        },
+    )
     def post(self, request, content_id):
         post = get_object_or_404(Post, content_id=content_id)
 
