d53d1465941a489139b595a61aadec0b097fb29e
simseulnyang <happysseul627@gmail.com>
2023-10-28 01:13:09 +0900

    :fire:Remove : 사용하지 않는 test코드 삭제

--- posts/tests.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.test import APITestCase
-
-
-class PostListViewTest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls
-        return super().setUpTestData()()
