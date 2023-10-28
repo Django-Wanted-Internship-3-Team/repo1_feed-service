d2c02fdeeee8c308cc2e85add2d76b81ac91f8f5
simseulnyang <happysseul627@gmail.com>
2023-10-28 19:52:02 +0900

    :bug:Fix : 중복된 import 삭제
    - user model에 대해 import를 2번 작성하여 중복되어 있음을 확인. 불필요한
      부분을 삭제했습니다.

--- posts/tests/views/test_posts_list_view.py
+++ posts/tests/views/test_posts_list_view.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
