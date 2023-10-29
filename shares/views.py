c3a1a5475ee07e0402be9f4fb3babd8842dc9505
chestnut90 <cowzon90@gmail.com>
2023-10-29 17:26:51 +0900

    :sparkles:Feat : 게시물 좋아요 api 추가 #12
    - 게시물 좋아요 api, /shares/<content_id>/ 에 대한 view 인터페이스 추가
    
    Related to #12

--- shares/views.py
+++ shares/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.views import APIView
+
+
+class SharesAPIView(APIView):
+    @swagger_auto_schema(operation_summary="게시물 좋아요")
+    def post(self, request, content_id):
+        pass
