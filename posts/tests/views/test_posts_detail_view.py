3578fb97e5655b173bbf2bc0dc29802d57f5f967
simseulnyang <happysseul627@gmail.com>
2023-10-30 01:13:31 +0900

    :white_check_mark:Test : 게시물 상세 조회 API Test코드 작성 #10
    - 해당 content_id가 존재하는 게시물 상세 조회 시 성공 케이스
    - 해당 content_id가 존재하지 않는 게시물 상세 조회 시 실패 케이스

---
+++ posts/tests/views/test_posts_detail_view.py
@@ -0,0 +1,60 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from posts.models import HashTag, Post
+
+User = get_user_model()
+
+
+class PostListViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # 테스트 사용자 생성
+        cls.user = User.objects.create_user(username="testuser1", password="testpass1", email="test1@email.com")
+
+        # 테스트 해시태그 생성
+        cls.hashtag = HashTag.objects.create(name="hashtag1")
+
+        # 테스트 게시물 생성 및 content_id 저장
+        cls.content_ids = []
+
+        # 테스트 게시물 생성
+        for i in range(1, 6):
+            post = Post.objects.create(
+                content_id=uuid.uuid4(),
+                user=cls.user,
+                post_type="facebook",
+                title=f"test title {i}",
+                content=f"test content {i}",
+                view_count=10 * i,
+                like_count=5 * i,
+                share_count=2 * i,
+                created_at=timezone.now(),
+                updated_at=timezone.now(),
+            )
+            post.hashtag.set([cls.hashtag])
+            cls.content_ids.append(str(post.content_id))
+
+    def setUp(self):
+        pass
+
+    def test_get_existing_post_content_id(self):
+        response = self.client.get(
+            path=reverse("post-detail", kwargs={"content_id": self.content_ids[0]}),
+            data={"type": "all", "hashtag": self.hashtag.id, "ordering": "created_at", "search": ""},
+        )
+        print(response.data)  # 서버의 응답 내용 출력
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_not_existing_post_content_id(self):
+        non_existing_content_id = "00000000-0000-0000-0000-000000000001"
+        response = self.client.get(
+            path=reverse("post-detail", kwargs={"content_id": non_existing_content_id}),
+            data={"type": "all", "hashtag": self.hashtag.id, "ordering": "created_at", "search": ""},
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
