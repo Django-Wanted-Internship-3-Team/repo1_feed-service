c5a7d05714064fcbc4fefa5e3dd0bc1bf727b59e
chestnut90 <cowzon90@gmail.com>
2023-10-30 14:46:11 +0900

    :hammer:Chore : 레포지토리 Root와 프로젝트 Root 분리
    - 개발 환경 구성을 위한 project root와 repository root 분리
    - repository root : git, docker, readme 등 환경 및 개발 구성 파일 포함
    - project root(src) : 장고 project 포함

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,7 @@
-from pathlib import Path
-import environ
 import os
+from pathlib import Path
+
+import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,12 +1,10 @@
-from rest_framework import permissions
-
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-from drf_yasg.views import get_schema_view
+from django.contrib import admin
+from django.urls import include, path
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
